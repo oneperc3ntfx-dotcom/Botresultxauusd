ff8d53d50d7cefc17fbd3ec5598e8901afed1dad
oneperc3ntfx-dotcom <oneperc3ntfx@gmail.com>
2026-05-04 03:19:18 +0700

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,12 +1,13 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+TELEGRAM_CHAT_ID = int(os.getenv("TELEGRAM_CHAT_ID"))
 
 FINNHUB_API_KEY = os.getenv("FINNHUB_API_KEY")
-SYMBOL = os.getenv("FINNHUB_SYMBOL", "XAUUSD")
+FINNHUB_SYMBOL = os.getenv("FINNHUB_SYMBOL", "XAUUSD")
 
 TIMEZONE = os.getenv("TIMEZONE", "Asia/Jakarta")
+
+# TOPIC IDS
+SIGNAL_TOPIC_ID = int(os.getenv("SIGNAL_TOPIC_ID", "0"))
+RESULT_TOPIC_ID = int(os.getenv("RESULT_TOPIC_ID", "0"))
